fe5cf0f019f06db0a68b0d44ab1c219506154dd4
Priyanshuojha234923 <143374227+Priyanshuojha234923@users.noreply.github.com>
2023-12-13 15:49:03 +0530

    13/12/2023

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,FormsModule,HomeComponent,LoginComponent,AboutComponent,ContactusComponent,RegistrationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Demoproject';
+  num:number=100;
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AboutComponent } from './about/about.component';
+import { RegistrationComponent } from './registration/registration.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path:"home",component:HomeComponent},
+   { path:"login",component:LoginComponent},
+   {path:"contactus",component:ContactusComponent},
+   {path:"about",component:AboutComponent},
+   {path:"registration",component:RegistrationComponent}
+
+
+];
